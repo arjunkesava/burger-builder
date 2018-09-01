1d926f76f236d37dfd20e1efeb4a666b8cf5493e
arjunkesava <arjun.kesava@gmal.com>
2018-09-01 18:20:24 +0530

    Rendered static Burger

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger.js'
 
 class BurgerBuilder extends Component {
   render() {
     return (
       <Aux>
-          <div>Burger</div>
-          <div>Burger Drawer</div>
+        <Burger />
       </Aux>
     )
   }
