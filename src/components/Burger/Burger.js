7d9f0b865a594cd82020ec729acec0762c507d68
arjunkesava <arjun.kesava@gmal.com>
2018-09-01 19:07:04 +0530

    Added burger ingredients state props

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,12 +2,25 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
-const burger = () => {
+const burger = (props) => {
+    let transformedIngredients =
+     Object.keys(props.ingredients)
+        .map(igKey => [...Array(props.ingredients[igKey])]
+            .map((_, i) =>
+                <BurgerIngredient key={igKey + i} type = {igKey} />
+            )
+        ).reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+    
+    if(!transformedIngredients.length){
+        transformedIngredients = <p>Please add burger ingredients from below</p>
+    }
+          
   return (
     <div className={classes.Burger}>
         <BurgerIngredient type="bread-top" />
-        <BurgerIngredient type="cheese" />
-        <BurgerIngredient type="meat" />
+        {transformedIngredients}
         <BurgerIngredient type="bread-bottom" />
     </div>
   );
